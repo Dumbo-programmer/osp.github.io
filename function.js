6cf830c9169e84a716b104aa89edf51783bbf6a8
Dumbo-Programmer <tawhidbinomarsjhss@gmail.com>
2022-01-21 21:43:21 +0600

    onul license

--- function.js
+++ function.js
@@ -6,13 +6,13 @@ opp.innerText = "----LICENSE START----"+
 "Year: <COPYRIGHT YEAR>    "+
     "Author: <Copyright Author>    "+
     "Software: <Software Author>"+
-    "******'Protected Under Open source protector Public License(OPL)'******"+
+    "******'Protected Under Open source protector Personal License(OPL)'******"+
     "This software is as it is without warranty of any kind."+
 
     "This License Allows::::"+
     "Anyone using the software is allowed to use,copy,modify,merge,publish and distribute within "+
     "LIMITATIONS.To deal with it is hereby granted but for individual only and this license must be included with every copy of this software"+
-    "Sub-Licensing is allowed"
+   // "Sub-Licensing is allowed"
     +"----LICENSE END----"
     }else if(op.value == "OCL"){
         opp.innerText = "----LICENSE START----"+
@@ -24,7 +24,7 @@ opp.innerText = "----LICENSE START----"+
 
     "This License Allows::::    "+
     "Anyone using the software is allowed to use,copy,modify,merge,publish and distribute without "+
-    "LIMITATIONS.To deal with it is hereby granted for both commercial and individual use cases  and this license must be included with every copy of this software."+
+    "LIMITATIONS.To deal with it is hereby granted for commercial and Trademark company use cases and this license must be included with every copy of this software."+
     "Sub-Licensing is allowed."+
     "SOURCE CODE including binaries must be published openly with  "+
     "*Modifier:<MODIFIER NAME>* this tag with the license and required value inplace of <>         "
@@ -41,10 +41,25 @@ opp.innerText = "----LICENSE START----"+
     "Anyone using the software is allowed to use,copy,modify,merge,publish and distribute without "+
     "LIMITATIONS[ONLY NON PROFIT ORGANIZATIONS].To deal with it is hereby granted for non-profit orgs and this license must be included with every copy of this software."+
     "Sub-Licensing is allowed."+
-    "SOURCE CODE including binaries must be published openly"+
+   // "SOURCE CODE including binaries must be published openly"+
     "   "
     +"----LICENSE END----"
  
+    }else if(op.value == "OPUL"){
+        opp.innerText = "----LICENSE START----"+
+        "Year: <COPYRIGHT YEAR>    "+
+    "Author: <Copyright Author>    "+
+    "Software: <Software Author>     "+
+    "******'Protected Under Open source protector Public License(OPUL)'******"+
+    "This software is as it is without warranty of any kind."+
+
+    "This License Allows::::    "+
+    "Anyone using the software is allowed to use,copy,modify,merge,publish and distribute without "+
+    "LIMITATIONS.To deal with it is hereby granted for both commercial and non commercial use cases  and this license must be included with every copy of this software."+
+    "Sub-Licensing is allowed."+
+    "SOURCE CODE including binaries must be published openly with  "+
+    "*Modifier:<MODIFIER NAME>* this tag with the license and required value inplace of <>         "
+    +"----LICENSE END----"
     }
     console.log(op.value)
   }
